59d3b29e6dee1f4687bc4af3dcd26b8cccf1fe92
MoonYoungMin <ansdudals8@naver.com>
2022-01-27 15:35:06 +0900

    update README

--- Q1.py
+++ Q1.py
@@ -1,34 +1,12 @@
-# greedy - 1. 모험가 길드
-# 내가 푼 방법
-n = int(input())
-afraid = list(map(int, input().split()))
-teams = 0
-afraid.sort()
+s = input()
+nums = list(s)
+first = int(nums[0])
 
-# 조건문에 순서 
-while True:
-    if len(afraid) == 0 :
-        break
-    elif len(afraid) < afraid[-1] :
-        break
-    elif len(afraid) >= afraid[-1] :
-        afraid = afraid[:len(afraid)-afraid[-1]]
-        teams += 1
+for i in range(1, len(nums)):
+    second = int(nums[i])
+    if first <= 1 or second <= 1: # 둘 중 하나라도 0이거나 1이면 더한다
+        first += second
+    else:
+        first *= second
 
-print(teams)
-
-# 책에서 풀이
-n = int(input())
-data = list(map(int,input().split()))
-data.sort()
-
-result = 0 # 총 그룹의 수
-count = 0 # 현재 그룹에 포함된 모험가의 수
-
-for i in data: # 공포도를 낮은 것부터 하나씩 확인하여
-    count += 1 # 현재 그룹에 해당 모험가를 포함시키자
-    if count >= i : # 현재 그룹에 포함된 모험가의 수가 현재의 공포도 이상이라면, 그룹 결성
-        result += 1 # 총 그룹의 수 증가시키기
-        count = 0 # 현재 그룹에 포함된 모험가의 수 초기화
-
-print(result)
+print(first)
